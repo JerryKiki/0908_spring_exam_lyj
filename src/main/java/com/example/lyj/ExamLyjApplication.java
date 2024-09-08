fa377b98719ebb7318db196da36db7828ffd37d1
JerryKiki <gasi758@gmail.com>
2024-09-08 19:43:00 +0900

    firstCommit

---
+++ src/main/java/com/example/lyj/ExamLyjApplication.java
@@ -0,0 +1,13 @@
+package com.example.lyj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamLyjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamLyjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lyj/ExamLyjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lyj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamLyjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
